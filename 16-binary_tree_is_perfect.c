f605cfe7a976c02a3ea52e2a79d793a7e5c59eee
JaysonPasquier <jaysonpasquier.contact@gmail.com>
2024-12-09 21:46:48 +0100

    task from number 9 to number 18, exe files are  in the exe directory and main.c files are in the directory called main

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise or if tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height, nodes, perfect_nodes;
+	size_t i;
+	size_t actual_nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	nodes = 1;
+
+	for (i = 0; i < height; i++)
+		nodes = nodes * 2;
+
+	perfect_nodes = nodes - 1;
+
+	if (tree->left)
+		actual_nodes += binary_tree_is_perfect(tree->left);
+	if (tree->right)
+		actual_nodes += binary_tree_is_perfect(tree->right);
+
+	return (actual_nodes == perfect_nodes);
+}
